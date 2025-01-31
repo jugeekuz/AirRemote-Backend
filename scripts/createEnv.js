725e2c1e6ad8f2fcc0dd52ddcdbb795d24c6da2d
Anastasios Diamantis <tdiamantis97@gmail.com>
2025-01-31 23:55:53 +0200

    Update post deployment script

--- scripts/createEnv.js
+++ scripts/createEnv.js
@@ -3,30 +3,67 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
 
+const configPath = "./config.json";
+
+const createAdmin = async (stage) => {
+
+  if (!fs.existsSync(configPath)) {
+    console.error("Error: config.json not found!");
+    process.exit(1);
+  }
+
+  const config = JSON.parse(fs.readFileSync(configPath, "utf-8"));
+  const { adminEmail, region } = config;
+  const tableName = `${stage}RegisteredUsers`;
+
+  if (!tableName) {
+    console.error("❌ Error: REGISTERED_USERS_TABLE_NAME is not set.");
+    process.exit(1);
+  }
+  console.log(`🛠️  Adding admin "${adminEmail}" to table "${tableName}" in region "${region}"...`);
+  const command = `aws dynamodb put-item --table-name "${tableName}" --item "{\\"userEmail\\": {\\"S\\": \\"${adminEmail}\\"}}" --region "${region}"`;
+  try {
+    const output = execSync(command, { encoding: "utf-8" });
+    console.log(`✅ Successfully added admin ${adminEmail} to registered users ${output}`);
+  } catch (error) {
+    console.error(`Error while creating admin: ${error.message}`);
+  }
+}
+
+
 (async () => {
   try {
-    const stage = process.argv[2] || 'dev';
+    const stage = process.argv[2] || 'dev'; 
+    // const output = execSync(`serverless info --stage ${stage}`).toString();
+    // const regex = /((https?|wss):\/\/[a-zA-Z0-9.-]+)\//g;
+    // const matches = new Set([...output.matchAll(regex)].map(match => match[1]));
+    // const urls = Array.from(matches);
 
-    const output = execSync('serverless info').toString();
+    if (!fs.existsSync(configPath)) {
+      console.error("Error: config.json not found!");
+      process.exit(1);
+    }
+  
+    const config = JSON.parse(fs.readFileSync(configPath, "utf-8"));
+    const { corsOrigin } = config;
+    const url = new URL(corsOrigin);
+    const domain = url.hostname;
+    
+    const envContent = 
+      `VITE_APP_CLIENT_ID="<YOUR-APP-CLIENT-ID>"\n` +
+      `VITE_COGNITO_DOMAIN="<YOUR-COGNITO-DOMAIN>"\n` +
+      `VITE_STAGE="${stage}"\n` +
+      `VITE_BASE_URL="${corsOrigin}"\n` +
+      `VITE_API_URL="https://api.${domain}/api"\n` +
+      `VITE_AUTH_URL="https://auth.${domain}/auth"\n` +
+      `VITE_WSS_URL="wss://wss.${domain}"\n`;
 
-    const regex = /((https?|wss):\/\/[a-zA-Z0-9.-]+)\//g;
-    
-    const matches = new Set([...output.matchAll(regex)].map(match => match[1]));
-    const urls = Array.from(matches);  
-    
-    const envContent = `VITE_STAGE="${stage}"\n` + 
-      urls
-      .map((url) => {
-        url.includes('https') ? 
-        `VITE_BASE_URL="${url}"
-        VITE_API_URL="${url}/api"
-        VITE_AUTH_URL="${url}/auth"
-        VITE_APP_CLIENT_ID="<YOUR-APP-CLIENT-ID>"
-        VITE_COGNITO_DOMAIN="<YOUR-COGNITO-DOMAIN>"
-        ` : 
-        `VITE_WSS_URL="${url}"`
-      })
-      .join('\n');
+
+
+    // const envContent = `VITE_STAGE="${stage}"\nVITE_APP_CLIENT_ID="<YOUR-APP-CLIENT-ID>"\nVITE_COGNITO_DOMAIN="<YOUR-COGNITO-DOMAIN>"\n` + 
+    //   urls
+    //     .map((url) => url.includes('https') ? `VITE_BASE_URL="${url}"\nVITE_API_URL="${url}/api"\nVITE_AUTH_URL="${url}/auth"` : `VITE_WSS_URL="${url}"\n`)
+    //     .join('\n');
 
     const envDir = './outputs';
     const envPath = './outputs/.env';
@@ -37,6 +74,7 @@ const { execSync } = require('child_process');
     fs.writeFileSync(envPath, envContent);
 
     console.log(`.env file created at ${envPath}`);
+    await createAdmin(stage);
   } catch (error) {
     console.error('Error generating .env file:', error);
   }
--- serverless.config.js
+++ serverless.config.js
@@ -1,13 +1,15 @@
 const config = require('./config.json');
 
-module.exports = {
-  ...config,
-  configFilePath: './config.json',
-
-  scriptable: {
-    hooks: {
-      'before:package:createDeploymentArtifacts': 'node ./scripts/predeploy.js',
-      'after:deploy:deploy': 'node ./scripts/createEnv.js'
+module.exports = (serverless) => {
+  const stage = serverless.options.stage || 'dev';
+  return {
+    ...config,
+    configFilePath: './config.json',
+    scriptable: {
+      hooks: {
+        'before:package:createDeploymentArtifacts': 'node ./scripts/predeploy.js',
+        'after:deploy:deploy': `node ./scripts/createEnv.js ${stage}`
+      }
     }
-  }
+  };
 };
